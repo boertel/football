f93f854517716e5e5aacd2879ba1e6a9d3423a26
boertel <benjamin.oertel@gmail.com>
2018-04-26 17:39:38 -0700

    no debug

--- football/settings.py
+++ football/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%s$$%!!e%kud9pa82t$)c@*alw+oh*tg$7tx49-kc%9=q#cn_p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'true'
 
 ALLOWED_HOSTS = []
 
