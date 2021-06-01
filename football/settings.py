d114eee4dfaef06e76c140198b6a38c1530b4cd1
boertel <ben@comediadesign.com>
2021-06-01 09:54:42 -0700

    add storages to apps

--- football/settings.py
+++ football/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "storages",
     "django.contrib.staticfiles",
     "corsheaders",
     "rest_framework",
